918c9651e33f91fe571a7e01714bc5011f8679f5
Christophe Larsonneur <christophe.larsonneur@contentsquare.com>
2020-01-21 20:33:15 +0100

    get roles and refactor

--- api.go
+++ api.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/clarsonneur/onelogin/api"
 	"github.com/clarsonneur/onelogin/common"
 	"github.com/op/go-logging"
 )
@@ -16,68 +17,31 @@ const (
 	MaxIterGetSAMLResponse     = 6
 )
 
-// API is the core API object
-type API struct {
-	// Allow custom base URL to override the generated URL
-	CustomURL string
+// Service is the core OneLogin service object, connected to the OneLogin Service through the API (api.Core).
+type Service struct {
+	core *api.Core
 
-	// OneLogin service shard (eu, us, etc)
-	Shard string
-
-	// Token struct for managing the OAuth token
-	Token *OAuthTokenResult
-
-	SubDomain    string
-	ClientID     string
-	ClientSecret string
 }
 
-// NewAPI create the main API object
-func NewAPI(shard string, clientID string, clientSecret string, subdomain string, loglevel logging.Level) *API {
-	ol := API{Shard: shard, ClientID: clientID, ClientSecret: clientSecret, SubDomain: subdomain}
-	ol.SetLogLevel(loglevel)
-	return &ol
-}
+// NewService create the main API object
+func NewService(shard string, clientID string, clientSecret string, subdomain string, loglevel logging.Level) (ret *Service) {
+	ret = new(Service)
+	ret.core = api.NewAPI(shard, clientID, clientSecret, subdomain)
+	ret.SetLogLevel(loglevel)
 
-// GetURL creates a URL given the URI and any given args.
-// Returns a URL
-func (o *API) GetURL(uri string, args ...string) string {
-	// Handle cases where the uri requires variable replacements (ie.  /api/1/user/%d/roles)
-	fulluri := uri
-
-	if len(args) > 0 {
-		// Convert to slice of interface so that the slice can be sent in as a variadic argument
-		argint := make([]interface{}, len(args))
-		for index, value := range args {
-			argint[index] = value
-		}
-		fulluri = fmt.Sprintf(uri, argint...)
-	}
-
-	if o.CustomURL != "" {
-		return fmt.Sprintf("%s/%s", o.CustomURL, fulluri)
-	}
-
-	return fmt.Sprintf("%s/%s", fmt.Sprintf(OneLoginURL, o.Shard), fulluri)
+	return
 }
 
 // SetLogLevel define the log level
-func (o *API) SetLogLevel(loglevel logging.Level) {
+func (o *Service) SetLogLevel(loglevel logging.Level) {
 	SetLogLevel(loglevel)
 }
 
-// ObtainAPIAccess initialize the access to the API.
-func (o *API) ObtainAPIAccess() (err error) {
-	o.Token = NewOAuthTokenResult()
-
-	return o.Token.Obtain(o)
-}
-
 // SAMLAuthenticate used to authenticate a user thanks to SAML
-func (o *API) SAMLAuthenticate(user, pass, appID, ip string, mfa, deviceIndex int) (result *AwsSAMLAssertion, err error) {
+func (o *Service) SAMLAuthenticate(user, pass, appID, ip string, mfa, deviceIndex int) (result *AwsSAMLAssertion, err error) {
 	result = NewAwsSAMLAssertion(user, pass)
-	assertion := NewSAMLAssertionResult()
-	_, err = assertion.Post(o, user, pass, appID, o.SubDomain, ip)
+	assertion := api.NewSAMLAssertionResult()
+	_, err = assertion.Post(o.core, user, pass, appID, o.core.SubDomain, ip)
 
 	if err != nil {
 		return
@@ -90,35 +54,37 @@ func (o *API) SAMLAuthenticate(user, pass, appID, ip string, mfa, deviceIndex in
 		result.SetDecoded(assertion.Data)
 		return
 	}
-	err = json.Unmarshal(assertion.Data, &assertion.data)
+
+	var data []api.SAMLAssertionDataResult
+	err = json.Unmarshal(assertion.Data, &data)
 	if err != nil {
 		return
 	}
 
 	// Display list of MFA Devices to use
 	fmt.Printf("\nMFA Required\n")
-	var device SAMLAssertionDevice
+	var device api.SAMLAssertionDevice
 	if deviceIndex == -1 {
 		fmt.Print("Authenticate using one of these devices:\n")
 		fmt.Print("-----------------------------------------------------------------------\n")
-		for index, device := range assertion.data[0].Devices {
+		for index, device := range data[0].Devices {
 			fmt.Printf(" %d | %s\n", index, device.DeviceType)
 		}
 		fmt.Print("-----------------------------------------------------------------------\n")
 
-		device = assertion.data[0].Devices[common.Select(0, len(assertion.data[0].Devices)-1)]
+		device = data[0].Devices[common.Select(0, len(data[0].Devices)-1)]
 	} else {
-		if deviceIndex >= len(assertion.data[0].Devices) {
-			err = fmt.Errorf("Invalid index %d. It must be between 0 and %d", deviceIndex, len(assertion.data[0].Devices)-1)
+		if deviceIndex >= len(data[0].Devices) {
+			err = fmt.Errorf("Invalid index %d. It must be between 0 and %d", deviceIndex, len(data[0].Devices)-1)
 			return
 		}
-		device = assertion.data[0].Devices[deviceIndex]
+		device = data[0].Devices[deviceIndex]
 		fmt.Printf("Using the MFA device index %d (%s)\n", deviceIndex, device.DeviceType)
 	}
 	result.MfaVerifyInfo.DeviceID = device.DeviceID
 	result.MfaVerifyInfo.DeviceType = device.DeviceType
 
-	verifyFactor := NewVerifyFactorResult()
+	verifyFactor := api.NewVerifyFactorResult()
 
 	var MFACode int
 
@@ -127,16 +93,16 @@ func (o *API) SAMLAuthenticate(user, pass, appID, ip string, mfa, deviceIndex in
 	switch device.DeviceType {
 	case "OneLogin SMS":
 		fmt.Printf("SMS with OTP token sent to device %d\n", device.DeviceID)
-		verifyFactor.Post(o, appID, device.DeviceID, assertion.data[0].StateToken, "", true)
+		verifyFactor.Post(o.core, appID, device.DeviceID, data[0].StateToken, "", true)
 		MFACode = common.GetNumber("Enter the SMS OTP code received:")
 	case "OneLogin Protect":
 		fmt.Printf("PUSH with OTP token sent to device %d\n", device.DeviceID)
-		_, err = verifyFactor.Post(o, appID, device.DeviceID, assertion.data[0].StateToken, "", false)
+		_, err = verifyFactor.Post(o.core, appID, device.DeviceID, data[0].StateToken, "", false)
 		// Push. Need to wait for OneLogin to confirm.
 		time.Sleep(time.Second * TimeSleepOnResponsePending)
 		for i := 0; i < MaxIterGetSAMLResponse; i++ {
 			fmt.Printf(".")
-			_, err = verifyFactor.Post(o, appID, device.DeviceID, assertion.data[0].StateToken, "", true)
+			_, err = verifyFactor.Post(o.core, appID, device.DeviceID, data[0].StateToken, "", true)
 			if err != nil {
 				return
 			}
@@ -151,7 +117,7 @@ func (o *API) SAMLAuthenticate(user, pass, appID, ip string, mfa, deviceIndex in
 			time.Sleep(time.Second * TimeSleepOnResponsePending)
 		}
 		fmt.Printf("\nUnable to get your device (%d) authentication.\n", device.DeviceID)
-		verifyFactor.Post(o, appID, device.DeviceID, assertion.data[0].StateToken, "", true)
+		verifyFactor.Post(o.core, appID, device.DeviceID, data[0].StateToken, "", true)
 		MFACode = common.GetNumber("Enter the OneProtect OTP code from your mobile application:")
 		return
 
@@ -164,7 +130,7 @@ func (o *API) SAMLAuthenticate(user, pass, appID, ip string, mfa, deviceIndex in
 		}
 	}
 	result.MfaVerifyInfo.OTPToken = MFACode
-	_, err = verifyFactor.Post(o, appID, device.DeviceID, assertion.data[0].StateToken, fmt.Sprintf("%d", MFACode), true)
+	_, err = verifyFactor.Post(o.core, appID, device.DeviceID, data[0].StateToken, fmt.Sprintf("%d", MFACode), true)
 	if verifyFactor.Status.Type == "success" {
 		result.SetDecoded([]byte(verifyFactor.Data))
 		return
@@ -172,10 +138,5 @@ func (o *API) SAMLAuthenticate(user, pass, appID, ip string, mfa, deviceIndex in
 	if verifyFactor.Status.Error {
 		err = fmt.Errorf("%s: %s", verifyFactor.Status.Type, verifyFactor.Status.Message)
 	}
-	return
-}
-
-func (o *API) getBearerHeaders() (ret common.Headers) {
-	ret = GetHeaders("bearer:" + o.Token.AccessToken)
 	return
 }
--- global.go
+++
@@ -1,9 +0,0 @@
-package onelogin
-
-// ResultStatus is the common result status
-type ResultStatus struct {
-	Type    string `json:"type"`
-	Message string `json:"message"`
-	Error   bool   `json:"error"`
-	Code    int    `json:"code"`
-}
