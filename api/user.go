14cce7cadf4246b9ed11e7ed2ce007592ee4af36
Christophe Larsonneur <christophe.larsonneur@contentsquare.com>
2020-01-23 11:44:00 +0100

    Add more fields needed to be decoded

--- api/user.go
+++ api/user.go
@@ -2,6 +2,7 @@ package api
 
 // User struct
 type User struct {
+	Username      string            `json:"username"`
 	Email         string            `json:"email"`
 	ID            int64             `json:"id"`
 	Status        int               `json:"status"`
@@ -9,5 +10,7 @@ type User struct {
 	RolesID       []int64           `json:"role_id"`
 	ManagerUserID int64             `json:"manager_user_id"`
 	MemberOf      string            `json:"member_of"`
+	Firstname     string            `json:"firstname"`
+	Lastname      string            `json:"lastname"`
 	CustomAttrs   map[string]string `json:"custom_attributes"`
 }
